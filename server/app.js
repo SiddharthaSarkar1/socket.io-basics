ea05a7905760a6a7f1becd27fcb26d6288ff0787
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-01-23 02:16:03 +0530

    establish connection between server and client

--- server/app.js
+++ server/app.js
@@ -1,13 +1,39 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000;
 
 const app = express();
+const server = createServer(app);
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+//io is curcuit's instance
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
-app.listen(port, () => {
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log("Id", socket.id);
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
